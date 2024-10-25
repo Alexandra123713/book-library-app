6b128fd202c8769901857fcbeda0d6fced79d509
Alexandra Cerevatii <alexandracerevatii@gmail.com>
2024-10-25 20:16:25 +0300

    Added thunkFunction

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,5 +1,6 @@
+import axios from 'axios'
 import { createSlice } from "@reduxjs/toolkit"
-import filterSlice from "./filterSlice"
+import createBookWithID from '../../utils/createBookWithID'
 
 const initialState = []
 
@@ -19,17 +20,24 @@ const booksSlice = createSlice({
                     book.isFavorite = !book.isFavorite
                 }
             })
-            // return state.map((book)=>
-            // book.id === action.payload
-            //  ? {...book, isFavorite: !book.isFavorite}
-            //  : book
-            // )
         }
     }
 })
 
 
 export const {addBook, deleteBook, toggleFavorite} = booksSlice.actions
+
+export const thunkFunction = async (dispatch, getState) => {
+    try {
+
+        const res = await axios.get('http://localhost:4000/random-book')
+        if (res?.data?.title && res?.data?.author) {
+            dispatch(addBook(createBookWithID(res.data, 'API')))
+        }
+    } catch(error) {
+        console.log('Error fetching random book', error)
+    }
+}
 
 export const selectBooks = (state) => state.books
 
